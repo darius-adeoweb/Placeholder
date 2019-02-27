03cf3c20670d52da17e19fc664ceaa96ec913003
darius-adeoweb <darius@adeoweb.lt>
2019-02-27 15:08:46 +0200

    update

---
+++ src/Providers/PlaceholderServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Placeholder\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class PlaceholderServiceProvider extends ServiceProvider
+{
+    /**
+     * Register service provider
+     */
+    public function register()
+    {
+
+    }
+}
